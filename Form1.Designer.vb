c461244be584789b61075aa57467da8ba7edecfe
lucilacuadra <lucilacuadra@gmail.com>
2023-02-06 21:18:58 -0300

    Se agrega el proyecto-ProyectoFinalista

---
+++ Form1.Designer.vb
@@ -0,0 +1,275 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtBuscador = New System.Windows.Forms.TextBox()
+        Me.lstDatos = New System.Windows.Forms.ListBox()
+        Me.txtTelefono = New System.Windows.Forms.TextBox()
+        Me.txtDireccion = New System.Windows.Forms.TextBox()
+        Me.btnBuscador = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.btnEditar = New System.Windows.Forms.Button()
+        Me.lblAgenda = New System.Windows.Forms.Label()
+        Me.BtnGuardar = New System.Windows.Forms.Button()
+        Me.BtnLeer = New System.Windows.Forms.Button()
+        Me.btnAgregar = New System.Windows.Forms.Button()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label6
+        '
+        Me.Label6.BackColor = System.Drawing.Color.SandyBrown
+        Me.Label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label6.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(48, 95)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(199, 23)
+        Me.Label6.TabIndex = 82
+        Me.Label6.Text = "Nombre"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.NavajoWhite
+        Me.Label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label5.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(48, 125)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(199, 23)
+        Me.Label5.TabIndex = 81
+        Me.Label5.Text = "Dirección"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.SystemColors.Info
+        Me.Label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label4.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(48, 155)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(199, 23)
+        Me.Label4.TabIndex = 80
+        Me.Label4.Text = "Teléfono"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.Khaki
+        Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(537, 191)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(217, 23)
+        Me.Label3.TabIndex = 79
+        Me.Label3.Text = "Teléfono"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Khaki
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(281, 191)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(229, 23)
+        Me.Label2.TabIndex = 78
+        Me.Label2.Text = "Dirección"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Khaki
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(48, 191)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(199, 23)
+        Me.Label1.TabIndex = 77
+        Me.Label1.Text = "Nombre"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtBuscador
+        '
+        Me.txtBuscador.Location = New System.Drawing.Point(267, 443)
+        Me.txtBuscador.Multiline = True
+        Me.txtBuscador.Name = "txtBuscador"
+        Me.txtBuscador.Size = New System.Drawing.Size(284, 24)
+        Me.txtBuscador.TabIndex = 76
+        '
+        'lstDatos
+        '
+        Me.lstDatos.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstDatos.FormattingEnabled = True
+        Me.lstDatos.ItemHeight = 14
+        Me.lstDatos.Location = New System.Drawing.Point(48, 217)
+        Me.lstDatos.Name = "lstDatos"
+        Me.lstDatos.ScrollAlwaysVisible = True
+        Me.lstDatos.Size = New System.Drawing.Size(706, 200)
+        Me.lstDatos.TabIndex = 75
+        '
+        'txtTelefono
+        '
+        Me.txtTelefono.Location = New System.Drawing.Point(253, 154)
+        Me.txtTelefono.Multiline = True
+        Me.txtTelefono.Name = "txtTelefono"
+        Me.txtTelefono.Size = New System.Drawing.Size(501, 24)
+        Me.txtTelefono.TabIndex = 74
+        '
+        'txtDireccion
+        '
+        Me.txtDireccion.Location = New System.Drawing.Point(253, 124)
+        Me.txtDireccion.Multiline = True
+        Me.txtDireccion.Name = "txtDireccion"
+        Me.txtDireccion.Size = New System.Drawing.Size(501, 24)
+        Me.txtDireccion.TabIndex = 73
+        '
+        'btnBuscador
+        '
+        Me.btnBuscador.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btnBuscador.Image = CType(resources.GetObject("btnBuscador.Image"), System.Drawing.Image)
+        Me.btnBuscador.Location = New System.Drawing.Point(557, 436)
+        Me.btnBuscador.Name = "btnBuscador"
+        Me.btnBuscador.Size = New System.Drawing.Size(44, 39)
+        Me.btnBuscador.TabIndex = 72
+        Me.btnBuscador.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Image = CType(resources.GetObject("btnEliminar.Image"), System.Drawing.Image)
+        Me.btnEliminar.Location = New System.Drawing.Point(194, 427)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(67, 53)
+        Me.btnEliminar.TabIndex = 71
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'btnEditar
+        '
+        Me.btnEditar.Image = CType(resources.GetObject("btnEditar.Image"), System.Drawing.Image)
+        Me.btnEditar.Location = New System.Drawing.Point(121, 426)
+        Me.btnEditar.Name = "btnEditar"
+        Me.btnEditar.Size = New System.Drawing.Size(67, 55)
+        Me.btnEditar.TabIndex = 70
+        Me.btnEditar.UseVisualStyleBackColor = True
+        '
+        'lblAgenda
+        '
+        Me.lblAgenda.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.lblAgenda.Font = New System.Drawing.Font("Old English Text MT", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAgenda.Location = New System.Drawing.Point(250, 35)
+        Me.lblAgenda.Name = "lblAgenda"
+        Me.lblAgenda.Size = New System.Drawing.Size(349, 31)
+        Me.lblAgenda.TabIndex = 69
+        Me.lblAgenda.Text = "Agenda Virtual"
+        Me.lblAgenda.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'BtnGuardar
+        '
+        Me.BtnGuardar.Image = CType(resources.GetObject("BtnGuardar.Image"), System.Drawing.Image)
+        Me.BtnGuardar.Location = New System.Drawing.Point(692, 427)
+        Me.BtnGuardar.Name = "BtnGuardar"
+        Me.BtnGuardar.Size = New System.Drawing.Size(62, 48)
+        Me.BtnGuardar.TabIndex = 68
+        Me.BtnGuardar.UseVisualStyleBackColor = True
+        '
+        'BtnLeer
+        '
+        Me.BtnLeer.Image = CType(resources.GetObject("BtnLeer.Image"), System.Drawing.Image)
+        Me.BtnLeer.Location = New System.Drawing.Point(616, 426)
+        Me.BtnLeer.Name = "BtnLeer"
+        Me.BtnLeer.Size = New System.Drawing.Size(70, 49)
+        Me.BtnLeer.TabIndex = 67
+        Me.BtnLeer.UseVisualStyleBackColor = True
+        '
+        'btnAgregar
+        '
+        Me.btnAgregar.Image = CType(resources.GetObject("btnAgregar.Image"), System.Drawing.Image)
+        Me.btnAgregar.Location = New System.Drawing.Point(48, 426)
+        Me.btnAgregar.Name = "btnAgregar"
+        Me.btnAgregar.Size = New System.Drawing.Size(67, 57)
+        Me.btnAgregar.TabIndex = 66
+        Me.btnAgregar.UseVisualStyleBackColor = True
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(253, 94)
+        Me.txtNombre.Multiline = True
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(501, 24)
+        Me.txtNombre.TabIndex = 65
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 536)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtBuscador)
+        Me.Controls.Add(Me.lstDatos)
+        Me.Controls.Add(Me.txtTelefono)
+        Me.Controls.Add(Me.txtDireccion)
+        Me.Controls.Add(Me.btnBuscador)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnEditar)
+        Me.Controls.Add(Me.lblAgenda)
+        Me.Controls.Add(Me.BtnGuardar)
+        Me.Controls.Add(Me.BtnLeer)
+        Me.Controls.Add(Me.btnAgregar)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtBuscador As TextBox
+    Friend WithEvents lstDatos As ListBox
+    Friend WithEvents txtTelefono As TextBox
+    Friend WithEvents txtDireccion As TextBox
+    Friend WithEvents btnBuscador As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents btnEditar As Button
+    Friend WithEvents lblAgenda As Label
+    Friend WithEvents BtnGuardar As Button
+    Friend WithEvents BtnLeer As Button
+    Friend WithEvents btnAgregar As Button
+    Friend WithEvents txtNombre As TextBox
+End Class
---
+++ Form1Proyectofinal.vb
@@ -0,0 +1,227 @@
+﻿Imports System.ComponentModel
+Imports System.IO
+Public Class Form1
+    Dim NomArchivo As String = "Datos.txt"
+    Dim Ubicacion As String = "C:\GUI\GuardarDatos"
+    Dim HayCambios As Boolean
+    Dim nombre As String
+    Dim direccion As String
+    Dim telefono As String
+    Dim Existe As Integer
+    Dim Contar As Integer
+    Dim Opc As Integer
+
+
+
+    Sub ControlarLimiteTextBox(nombre As String, direccion As String, telefono As String)
+
+        If (nombre.Length > 30 OrElse direccion.Length > 30 OrElse telefono.Length > 15) Then
+            MsgBox("El nombre o direccion no puede tener mas de 30 caracteres y el telefono mas de 15 !!", vbCritical)
+            Me.txtNombre.Text = ""
+            Me.txtDireccion.Text = ""
+            Me.txtTelefono.Text = ""
+            Me.txtNombre.Focus()
+        Else
+            Me.txtNombre.Text = Mid(Me.txtNombre.Text, 1, 30)
+            Me.txtDireccion.Text = Mid(Me.txtDireccion.Text, 1, 30)
+            Me.txtTelefono.Text = Mid(Me.txtTelefono.Text, 1, 15)
+            lstDatos.Items.Add(Space(8) + txtNombre.Text + Space(52 - Len(txtNombre.Text)) + " " + txtDireccion.Text + Space(52 - Len(txtDireccion.Text)) + " " +
+                               txtTelefono.Text)
+            txtNombre.Text = ""
+            txtDireccion.Text = ""
+            txtTelefono.Text = ""
+            txtNombre.Focus()
+            HayCambios = True
+
+        End If
+        Exit Sub
+    End Sub
+
+    Sub ControlarQueContengaCaracteresTexbox(nombre As String, direccion As String, telefono As String)
+        If (nombre.ToString = "" OrElse direccion.ToString = "" OrElse telefono.ToString = "") Then
+            MsgBox("¡Debe completar nombre, apellido y un teléfono!", vbCritical)
+            Me.txtNombre.Text = ""
+            Me.txtDireccion.Text = ""
+            Me.txtTelefono.Text = ""
+            Me.txtNombre.Focus()
+        End If
+        Exit Sub
+    End Sub
+
+    Sub Guardar(Archivo As String)
+        Dim GrabarArchivo As New StreamWriter(Archivo)
+        For i = 0 To lstDatos.Items.Count - 1
+            Dim Linea = lstDatos.Items(i).ToString()
+            GrabarArchivo.WriteLine(Linea)
+        Next
+
+        GrabarArchivo.Close()
+        MsgBox("Archivo grabado Correctamente", vbInformation)
+        HayCambios = False
+        txtNombre.Focus()
+    End Sub
+
+
+    Sub Leer(Archivo As String)
+        Dim leerArchivo As New StreamReader(Archivo)
+        lstDatos.Items.Clear()
+        Try
+            While leerArchivo.Peek <> -1
+                Dim linea As String = leerArchivo.ReadLine
+                If String.IsNullOrEmpty(linea) Then
+                    Continue While
+                End If
+
+                lstDatos.Items.Add(linea)
+            End While
+
+            leerArchivo.Close()
+        Catch ex As Exception
+            MsgBox("Se presento un problema al leer el archivo: " & ex.Message, MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+    Sub Crear(Archivo As String)
+        Dim CrearArchivo As FileStream = File.Create(Archivo)
+        CrearArchivo.Close()
+        Me.BtnLeer.Visible = True
+        Me.BtnGuardar.Visible = True
+
+    End Sub
+
+    Private Sub lstDatos_SelectedIndexChanged_1(sender As Object, e As EventArgs)
+        HayCambios = True
+    End Sub
+
+
+    Private Sub Form1_Load_1(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.txtBuscador.Visible = False
+        HayCambios = False
+    End Sub
+
+    Private Sub Form1_Closing_1(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        If HayCambios Then
+            If MsgBox("No se a guardado los ultimos cambios . ¿Desea guardarlos?", vbYesNo + vbCritical) = vbYes Then
+                Me.Guardar(Ubicacion & NomArchivo)
+                e.Cancel = True
+            End If
+        End If
+    End Sub
+
+
+    Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
+
+        If txtNombre.Text = "" Then
+            MsgBox("Ingresar Nombre", vbCritical)
+            txtNombre.Focus()
+            Exit Sub
+        ElseIf txtDireccion.Text = "" Then
+            MsgBox("Ingresar Dirección", vbCritical)
+            txtDireccion.Focus()
+            Exit Sub
+        ElseIf txtTelefono.Text = "" Then
+            MsgBox("Ingresar Telefono", vbCritical)
+            txtTelefono.Focus()
+            Exit Sub
+        End If
+        nombre = Me.txtNombre.Text
+        direccion = Me.txtDireccion.Text
+        telefono = Me.txtTelefono.Text
+
+        txtNombre.Text = Mid(txtNombre.Text, 1, 30)
+        txtDireccion.Text = Mid(txtDireccion.Text, 1, 30)
+        txtTelefono.Text = Mid(txtTelefono.Text, 1, 15)
+
+        lstDatos.Items.Add("       " + nombre & Space(36 - Len(txtNombre.Text)) & direccion & Space(35 - Len(txtDireccion.Text)) & telefono & Space(20 - Len(txtTelefono.Text)))
+        txtNombre.Text = ""
+        txtDireccion.Text = ""
+        txtTelefono.Text = ""
+
+        ControlarQueContengaCaracteresTexbox(nombre, direccion, telefono)
+        HayCambios = True
+    End Sub
+
+    Private Sub btnEditar_Click(sender As Object, e As EventArgs) Handles btnEditar.Click
+
+        If lstDatos.SelectedIndex = -1 Then
+
+            MsgBox("Seleccione la linea que desee editar", vbCritical)
+            txtNombre.Focus()
+            Exit Sub
+        End If
+
+        nombre = Mid(lstDatos.SelectedItem.ToString, 8, 35)
+        direccion = Mid(lstDatos.SelectedItem.ToString, 44, 34)
+        telefono = Mid(lstDatos.SelectedItem.ToString, 79, 13)
+
+        Me.txtNombre.Text = nombre & Space(0 - Len(txtNombre.Text))
+        Me.txtDireccion.Text = direccion & Space(0 - Len(txtDireccion.Text))
+        Me.txtTelefono.Text = telefono & Space(0 - Len(txtTelefono.Text))
+
+        lstDatos.Items.Remove(lstDatos.SelectedItem.ToString)
+        HayCambios = True
+        txtNombre.Focus()
+
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles BtnGuardar.Click
+        If Me.lstDatos.Items.ToString = "" Then
+            MsgBox("Debe contener al menos un caracter", vbCritical,
+                   "Ingrese Informacion")
+            Exit Sub
+        End If
+        Me.Guardar(Ubicacion & NomArchivo)
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+
+        If lstDatos.SelectedIndex = -1 Then
+            MsgBox("Debe seleccionar un items para modificarlo", vbCritical)
+            txtNombre.Focus()
+            Exit Sub
+        End If
+
+        Dim opc As Integer
+        opc = MsgBox("Desea eliminaro", vbYesNo)
+        If opc = vbYes Then
+            lstDatos.Items.Remove(lstDatos.SelectedItem.ToString())
+        End If
+        lstDatos.ClearSelected()
+        txtNombre.Focus()
+
+    End Sub
+
+    Private Sub btnBuscador_Click(sender As Object, e As EventArgs) Handles btnBuscador.Click
+        Static contadorClikcs As Integer
+        contadorClikcs += 1
+        If contadorClikcs = 1 Then
+            Me.txtBuscador.Visible = True
+        ElseIf contadorClikcs = 2 Then
+            contadorClikcs = 0
+            Me.txtBuscador.Visible = False
+        End If
+    End Sub
+
+    Private Sub txtBuscador_TextChanged(sender As Object, e As EventArgs) Handles txtBuscador.TextChanged
+        Dim i As Integer
+        Dim existe As Integer
+        If Len(Me.txtBuscador.Text) > 0 Then
+            For i = 0 To lstDatos.Items.Count - 1
+                existe = lstDatos.Items(i).indexOf(txtBuscador.Text)
+
+                If existe <> -1 Then
+                    Me.lstDatos.SelectedIndex() = i
+                    Exit For
+                End If
+            Next i
+        End If
+    End Sub
+
+    Private Sub txtTelefono_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtTelefono.KeyPress
+        If Not Char.IsNumber(e.KeyChar) And e.KeyChar <> Convert.ToChar(Keys.Back) Then
+            MsgBox("Solo se permite numeros", vbInformation, "Atencion")
+            e.Handled = True
+        End If
+    End Sub
+End Class
+
